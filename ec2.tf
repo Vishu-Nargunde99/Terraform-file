a3267fb71f7928f0c8a4921e90f554bf5ba7ee79
Vishakha Nargunde <vishakhanargunde1999@gmail.com>
2025-08-23 20:02:46 +0530

    chnages made in ec2.tf file

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+  
+}
+
 resource "aws_instance" "my_first_instnace" {
     ami = "ami-00ca32bbc84273381" #Arguments
     instance_type = "t2.micro" #Arguments
     tags = {
       name = "web-server"
     }
-    
-  
+
 }
