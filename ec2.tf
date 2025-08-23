9f95d767dab8dcec22be20669d0116456bd4233b
Vishakha Nargunde <vishakhanargunde1999@gmail.com>
2025-08-23 20:06:42 +0530

    modified

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 resource "aws_instance" "my_first_instnace" {
     ami = "ami-00ca32bbc84273381" #Arguments
-    instance_type = "t2.micro" #Arguments
+    instance_type = "t3.micro" #Arguments
     tags = {
       name = "web-server"
     }
