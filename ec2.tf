e2231c7d84cc58e17a73441971dc5f4dfcc837db
Vishakha Nargunde <vishakhanargunde1999@gmail.com>
2025-08-23 20:09:28 +0530

    modified ec2.tf file

--- ec2.tf
+++ ec2.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "my_first_instnace" {
     ami = "ami-00ca32bbc84273381" #Arguments
     instance_type = "t3.micro" #Arguments
     tags = {
-      name = "web-server"
+      Name = "web-server"
     }
 
 }
